2efb4990a856fc442d6c6b16eaa8e142be1979d3
madmen2529 <crazycode2529@gmail.com>
2020-01-16 16:07:06 +0700

    Edit code.

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync({ force: false }).then(() => {
   studentService(app, db);
   teacherService(app, db);
   examService(app, db);
